9c51e3ff3d12aa3c8687192eee27929569dfff78
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-03-11 21:34:00 +0200

    add some problems which task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle"
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+player.on('play', function() {
+        console.log('played the video!');
+    });
 
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+const onPlay = function (data) {
+    console.log(data.seconds)
+};
+const curentTime = data.seconds
+player.on('timeupdate', onPlay);
+localStorage.setItem("videoplayer-current-time", "data.seconds");
+player.setCurrentTime(curentTime)
+
+// document.addEventListener(
+//   "timeupdate",
+//   _.throttle(() => {
+//     console.log("Scroll handler call every 300ms");
+//   }, 100000)
+// );
